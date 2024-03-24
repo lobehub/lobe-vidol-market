713fae73d63c92243669a46cc14e2749b6d9260d
rdmclin2 <rdmclin2@gmail.com>
2024-03-24 22:23:08 +0800

    fix: test workflow

--- scripts/test.ts
+++ scripts/test.ts
@@ -2,16 +2,25 @@ import { consola } from "consola";
 import { readJSONSync } from "fs-extra";
 import { resolve } from "node:path";
 
-import { formatAndCheckSchema } from "./check";
-import { agents, agentsDir, root } from "./const";
+import { formatAgentSchema, formatDanceSchema } from "./check";
+import { agentFiles, agentsDir, root, danceFiles, dancesDir } from "./const";
 
 const runTest = () => {
-  for (const file of agents) {
+  for (const file of agentFiles) {
     if (file.isFile()) {
       const filePath = resolve(agentsDir, file.name);
       consola.start(filePath.replace(root, ""));
       const agent = readJSONSync(filePath);
-      formatAndCheckSchema(agent);
+      formatAgentSchema(agent);
+    }
+  }
+
+  for (const file of danceFiles) {
+    if (file.isFile()) {
+      const filePath = resolve(dancesDir, file.name);
+      consola.start(filePath.replace(root, ""));
+      const agent = readJSONSync(filePath);
+      formatDanceSchema(agent);
     }
   }
 };
