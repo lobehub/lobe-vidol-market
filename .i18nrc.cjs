6537439a90c9078e389a2f74c188a2e461e80801
Elegy <270663716@qq.com>
2024-07-16 23:44:19 +0800

    :sparkles:feat: update i18n

--- .i18nrc.cjs
+++ .i18nrc.cjs
@@ -9,4 +9,8 @@ module.exports = {
       return getDefaultExtension(locale);
     },
   },
+  selectors: ['meta.title', 'meta.description', 'meta.tags', 'config.systemRole'],
+  entryLocale: 'en-US',
+  outputLocales: ['zh-CN', 'en-US'],
+  modelName: 'gpt-3.5-turbo-0125',
 };
