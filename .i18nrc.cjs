f18dfb8b7a60fe9e2be1ea2145c2c1b30efc931b
Elegy <270663716@qq.com>
2024-08-03 23:43:57 +0800

    :sparkles: reset i18nrc.js

--- .i18nrc.cjs
+++ .i18nrc.cjs
@@ -4,12 +4,15 @@ module.exports = {
     entryLocale: "zh-CN",
     entryExtension: ".zh-CN.md",
     outputLocales: ["en-US"],
-    outputExtensions: (locale, { getDefaultExtension }) => {
+    outputExtensions: (locale, {
+      getDefaultExtension
+    }) => {
       if (locale === "en-US") return ".md";
       return getDefaultExtension(locale);
     },
   },
-  selectors: ['meta.title', 'meta.description', 'meta.tags', 'config.systemRole'],
+  selectors: ['meta.name','meta.readme','meta.description', 'systemRole','greeting','touch.head','touch.arm','touch.leg','touch.chest','touch.belly'],
+  // selectors: ['meta', 'systemRole', 'greeting', 'touch'],
   entryLocale: 'en-US',
   outputLocales: ['zh-CN', 'en-US'],
   modelName: 'gpt-3.5-turbo-0125',
